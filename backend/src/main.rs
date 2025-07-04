877737eb5488069f83a56f9e8beb03f7da02bea2
magwoo <magwood51@yandex.ru>
2025-07-04 08:17:31 +0300

    prepared minimal project

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
+use bcrypt::Version;
+use bcrypt::hash_with_result;
+
 fn main() {
-    println!("Hello, world!");
+    let hash = hash_with_result("assword123", 10)
+        .unwrap()
+        .format_for_version(Version::TwoA);
+
+    let mut split = hash.split('$').skip(1);
+
+    let algo = split.next().unwrap();
+    let cost = split.next().unwrap();
+    let rest = split.next().unwrap();
+
+    println!("rest: {}", rest);
+
+    let salt = &rest[..22];
+    let hash = &rest[22..];
+
+    println!("hash: {}, salt: {}", hash, salt);
 }
