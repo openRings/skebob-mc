41179a262999f570ec4f4bfcd14cc6b3da085199
magwoo <magwood51@yandex.ru>
2025-07-05 06:13:51 +0300

    update migrations: added sessions table

---
+++ backend/migrations/1_sessions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE sessions (
+    `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `access_token_hash` VARCHAR(255) NOT NULL,
+    `refresh_token_hash` VARCHAR(255) NOT NULL,
+    `user_id` BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    `expires_at` TIMESTAMP NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP)
+);
+
