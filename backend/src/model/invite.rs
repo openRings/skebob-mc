4a2b1560950c1827da580d9291e62617e5e189dd
magwoo <magwood51@yandex.ru>
2025-07-09 19:34:23 +0300

    update invite model struct: removed needless fields

--- backend/src/model/invite.rs
+++ backend/src/model/invite.rs
@@ -1,12 +1,11 @@
-use chrono::{DateTime, Utc};
 use sqlx::FromRow;
 
 #[derive(FromRow, Clone)]
 pub struct Invite {
     id: u64,
-    created_by: u64,
-    code: String,
-    created_at: DateTime<Utc>,
+    // created_by: u64,
+    // code: String,
+    // created_at: DateTime<Utc>,
 }
 
 impl Invite {
