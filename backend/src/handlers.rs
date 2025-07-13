ac17bb30c80bf6d60e75e904af5bdbeaee2f25c6
magwoo <magwood51@yandex.ru>
2025-07-13 08:47:40 +0300

    added grafana with prometheus as test

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -1,5 +1,8 @@
-use axum::{Router, middleware::from_fn};
+use axum::Router;
+use axum::middleware::from_fn;
+use axum::routing::get;
 use axum_cookie::CookieLayer;
+use axum_prometheus::PrometheusMetricLayerBuilder;
 
 use crate::database::Database;
 
@@ -11,11 +14,18 @@ mod invites;
 mod profile;
 
 pub fn create_router(state: Database) -> Router {
+    let (metric_layer, metric_handle) = PrometheusMetricLayerBuilder::new()
+        .enable_response_body_size(true)
+        .with_default_metrics()
+        .build_pair();
+
     Router::new()
         .nest("/invites", invites::get_nest())
         .nest("/profile", profile::get_nest())
         .merge(auth::get_nest())
+        .route("/metrics", get(|| async move { metric_handle.render() }))
         .layer(from_fn(error::error_logging_middleware))
         .layer(CookieLayer::strict())
+        .layer(metric_layer)
         .with_state(state)
 }
